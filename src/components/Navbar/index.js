ffbbfa8922aaebdce7ffb17cf2f9f8e2ce390bf8
devMarilia <devMarilia.frontend@gmail.com>
2022-08-01 21:09:12 -0300

    ✨ feat: Create Menu responsive

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useState} from 'react';
+import { Link } from 'react-router-dom';
+import { NavbarConteiner } from './styles'
 
-const Navbar = () => {
+function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+  const  menuItems = [
+    {
+      name: 'Home',
+      link: '/',
+    },
+    {
+      name: 'About',
+      link: '/about',
+    },
+    {
+      name: 'Product',
+      link: '/product',
+    },
+
+    ];
+
   return (
-    <div>Navbar</div>
+    <NavbarConteiner>
+      <span>
+        <i className="fas fa-bars" onClick={() => setIsOpen(!isOpen)} />
+      </span>
+        <div className={`nav-items ${isOpen && "open"}`}>
+          {menuItems.map(item => (
+            <li key={item.name}>
+                <Link to={item.link}>{item.name}</Link>
+            </li>
+            ))}
+        </div>
+        <div className={`nav-toggle ${isOpen && "open"}`}
+        onClick={() => setIsOpen(!isOpen)}>
+          <div className='bar'>
+
+          </div>
+        </div>
+    </NavbarConteiner>
   )
 }
-
 export default Navbar
